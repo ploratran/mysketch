dd2295dab311e7b69311147057eb4693fc30f1d2
Plora Tran <ploratran@gmail.com>
2022-02-14 09:54:16 -0800

    sticky header on homepage UI

---
+++ client/src/components/Feed.tsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+import { Container, Header } from "semantic-ui-react"; 
+
+export default function HomeContent() {
+    return (
+        <Container text style={{ marginTop: '7em' }}>
+            <Header as='h1'>Images will be displayed here</Header>
+        </Container>
+    )
+}
