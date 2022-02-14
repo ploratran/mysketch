ee9e9f1cd79aae7fde7cf649781a0f6ec520ea47
Plora Tran <ploratran@gmail.com>
2022-02-14 12:13:19 -0800

    implement sticky header with icons aligned center

---
+++ client/src/components/StickyHeader.tsx
@@ -0,0 +1,49 @@
+// sticky header includes Home button, search bar, profile, upload, feed, messenger buttons
+import React from "react";
+import { Menu, Dropdown, Container, Icon, Search } from "semantic-ui-react";
+
+export default function StickyHeader() {
+    return (
+        <Menu fixed="top" borderless>
+            <Container text> 
+                <Menu.Menu position="left">
+                    <Menu.Item header as="a">
+                        <h2>mysketch</h2>
+                    </Menu.Item>
+
+                    <Menu.Item header>
+                            <Search size='tiny' icon='search' placeholder='Search...' />
+                    </Menu.Item>
+                </Menu.Menu>
+
+                <Menu.Menu position="right">
+                    
+                    <Menu.Item as="a">
+                        <Icon name='home' size='large' />
+                    </Menu.Item>
+                    <Menu.Item as="a">
+                        <Icon name='rocketchat' size='large' />
+                    </Menu.Item>
+                    <Menu.Item as="a">
+                        <Icon name='plus square outline' size='large' />
+                    </Menu.Item>
+                
+                    <Dropdown item pointing text="User">
+                        <Dropdown.Menu>
+                            <Dropdown.Item>
+                                <Icon name='user circle' />
+                            </Dropdown.Item>
+
+                            <Dropdown.Divider />
+
+                            <Dropdown.Item>
+                                Logout
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+
+            </Container>
+        </Menu>
+    )
+};
